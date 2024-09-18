215e6aac462737dc64e3f66b79c584cd25179433
ayanaibraeva <ayanafront@gmail.com>
2024-09-18 16:23:30 +0600

    save

--- src/modules/HitProductsModule/components/Slide/Slide.jsx
+++ src/modules/HitProductsModule/components/Slide/Slide.jsx
@@ -1,57 +1,76 @@
 import classes from './Slide.module.sass';
-
-import  { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Typography } from "../../../../UI/Typography/Typography.jsx";
 
-const Slide = ({ images, name, description }) => (
-    <div className={classes.slideContent}>
-        <div className={classes.slideImage}>
-            <img src={images} alt={name}/>
+const Slide = ({ images, name, description }) => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const totalImages = images.length;
+
+    const handlePrevImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : totalImages - 1));
+    };
+    const handleNextImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex < totalImages - 1 ? prevIndex + 1 : 0));
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % totalImages);
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, [totalImages]);
+
+    return (
+        <div className={classes.slideContent}>
+            <div className={classes.slideImage}>
+                <button className={classes.sliderButtonPrev} onClick={handlePrevImage}>
+                    ‹
+                </button>
+                <img src={images[currentImageIndex]} alt={name} />
+                <button className={classes.sliderButtonNext} onClick={handleNextImage}>
+                    ›
+                </button>
+            </div>
+            <div className={classes.slideText}>
+                <Typography variant="h3">{name}</Typography>
+                <Typography className={classes.slideTextTitle} variant="bodyL">{description}</Typography>
+            </div>
+            {/* Removed image indicator */}
         </div>
-        <div className={classes.slideText}>
-            <Typography  variant="h3">{name}</Typography>
-            <Typography className={classes.slideTextTitle} variant="bodyL">{description}</Typography>
-        </div>
-    </div>
-);
+    );
+};
 
 const Slider = ({ slides }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
     const totalSlides = slides.length;
 
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : totalSlides - 1));
+    const handlePrevSlide = () => {
+        setCurrentSlideIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : totalSlides - 1));
     };
 
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex < totalSlides - 1 ? prevIndex + 1 : 0));
+    const handleNextSlide = () => {
+        setCurrentSlideIndex((prevIndex) => (prevIndex < totalSlides - 1 ? prevIndex + 1 : 0));
     };
-
-    const showPrevButton = currentIndex > 0;
-    const showNextButton = currentIndex < totalSlides - 1;
 
     return (
         <div className={classes.sliderContainer}>
-            {showPrevButton && (
-                <button className={classes.sliderButtonPrev} onClick={handlePrev}>
-                    ‹
-                </button>
-            )}
+            <button className={classes.sliderButtonPrev} onClick={handlePrevSlide}>
+                ‹
+            </button>
             <div className={classes.sliderWrapper}>
-                <Slide {...slides[currentIndex]} />
+                <Slide {...slides[currentSlideIndex]} />
             </div>
-            {showNextButton && (
-                <button className={classes.sliderButtonNext} onClick={handleNext}>
-                    ›
-                </button>
-            )}
+            <button className={classes.sliderButtonNext} onClick={handleNextSlide}>
+                ›
+            </button>
 
             <div className={classes.indicatorContainer}>
                 {slides.map((_, index) => (
                     <div
                         key={index}
-                        className={`${classes.indicator} ${index === currentIndex ? classes.activeIndicator : ''}`}
-                        onClick={() => setCurrentIndex(index)}
+                        className={`${classes.indicator} ${index === currentSlideIndex ? classes.activeIndicator : ''}`}
+                        onClick={() => setCurrentSlideIndex(index)}
                     />
                 ))}
             </div>
